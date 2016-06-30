20787d1ad5079615ae372055b94b20a3a99b7f71
myobie <me@nathanherald.com>
2016-06-30 12:34:08 +0200

    Can now control volume and wave type

--- app.js
+++ app.js
@@ -2,34 +2,60 @@ const Context = window.AudioContext || window.webkitAudioContext || window.mozAu
 const context = new Context()
 
 const oscillator = context.createOscillator()
-oscillator.frequency.value = 440
 
 const amp = context.createGain()
 amp.gain.value = 0
+
+let volume = 0
 
 oscillator.connect(amp)
 amp.connect(context.destination)
 oscillator.start(0)
 
-function down (frequency) {
+function updateNoteFrequency () {
+  const frequency = frequencyField.value
   const now = context.currentTime
   oscillator.frequency.setValueAtTime(frequency, now)
-  amp.gain.cancelScheduledValues(now)
-  amp.gain.setValueAtTime(amp.gain.value, now)
-  amp.gain.linearRampToValueAtTime(0.5, context.currentTime + 0.1)
 }
 
-function up () {
+function fadeIn () {
   const now = context.currentTime
-  amp.gain.cancelScheduledValues(now)
+  amp.gain.setValueAtTime(amp.gain.value, now)
+  amp.gain.linearRampToValueAtTime(volume, context.currentTime + 0.1)
+}
+
+function fadeOut () {
+  const now = context.currentTime
   amp.gain.setValueAtTime(amp.gain.value, now)
   amp.gain.linearRampToValueAtTime(0, context.currentTime + 0.1)
 }
 
-const playButton = document.getElementById('play-button')
+function down () {
+  updateNoteFrequency()
+  fadeIn()
+}
+
+function up () {
+  fadeOut()
+}
+
+function updateOscillatorType () {
+  oscillator.type = typeField.value
+}
+
 const frequencyField = document.getElementById('frequency-field')
+const volumeField = document.getElementById('volume-field')
+const typeField = document.getElementById('oscillator-type')
+const playButton = document.getElementById('play-button')
+
+volumeField.addEventListener('change', event => { volume = volumeField.value })
+volume = volumeField.value
+
+typeField.addEventListener('change', updateOscillatorType)
+updateOscillatorType()
+updateNoteFrequency()
 
 playButton.addEventListener('click', event => {
-  down(frequencyField.value)
+  down()
   setTimeout(up, 1000)
 })
