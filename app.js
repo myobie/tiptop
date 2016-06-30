8f4d602b52314490ca35c2745e23cda9e719ed2b
myobie <me@nathanherald.com>
2016-06-30 15:01:07 +0200

    Better player UI and some more unique sounds

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const repoField = document.getElementById('repo')
 const volumeField = document.getElementById('volume-field')
 const typeField = document.getElementById('oscillator-type')
 const playButton = document.getElementById('play-button')
+const stopButton = document.getElementById('stop-button')
 
 volumeField.addEventListener('change', event => { volume = volumeField.value })
 volume = volumeField.value
@@ -36,7 +37,7 @@ function createGithubURI () {
   let repo = repoField.value
   if (repo === '') { repo = 'tiptop' }
 
-  return `https://api.github.com/repos/${user}/${repo}/events`
+  return `https://api.github.com/repos/${user}/${repo}/events?per_page=100`
 }
 
 function fetchEvents () {
@@ -73,11 +74,77 @@ const types = {
   WatchEvent: [600, 0.4]
 }
 
+const typeAdjustments = {
+  CommitCommentEvent: note => note,
+  CreateEvent: note => note,
+  DeleteEvent: note => note,
+  DeploymentEvent: note => note,
+  DeploymentStatusEvent: note => note,
+  DownloadEvent: note => note,
+  FollowEvent: note => note,
+  ForkEvent: ([frequency, duration], event) => {
+    frequency = Math.floor(frequency * event.bias / 100.0) + 200
+    return [frequency, duration]
+  },
+  ForkApplyEvent: note => note,
+  GistEvent: note => note,
+  GollumEvent: note => note,
+  IssueCommentEvent: ([frequency, duration], event) => {
+    frequency = frequency + Math.floor(event.bias)
+    return [frequency, duration]
+  },
+  IssuesEvent: note => note,
+  MemberEvent: note => note,
+  MembershipEvent: note => note,
+  PageBuildEvent: note => note,
+  PublicEvent: note => note,
+  PullRequestEvent: note => note,
+  PullRequestReviewCommentEvent: ([frequency, duration], event) => {
+    duration *= event.bias / 50.0
+    return [frequency, duration]
+  },
+  PushEvent: note => note,
+  ReleaseEvent: note => note,
+  RepositoryEvent: note => note,
+  StatusEvent: note => note,
+  TeamAddEvent: note => note,
+  WatchEvent: ([frequency, duration], event) => {
+    if (event.payload.action === 'started') {
+      duration += event.bias / 100.0
+    }
+    return [frequency, duration]
+  }
+}
+
 const silence = [0, 0.5]
+
+function parseNote (event) {
+  let [frequency, duration] = types[event.type] || silence
+
+  // general adjustments
+
+  const actorId = event.actor.id
+  const actorIdString = actorId.toString()
+  const bias = (actorIdString.length * 5) + parseInt(actorIdString.substr(actorIdString.length - 3, 2), 10)
+  event.bias = bias
+
+  if (actorId < 1000) {
+    frequency -= bias
+    duration += 0.2
+  } else if (actorId > 500000) {
+    frequency += bias
+  }
+
+  // type specific adjustments
+
+  const adjustment = typeAdjustments[event.type] || (note => { return note })
+
+  return adjustment([frequency, duration], event)
+}
 
 function parseNotes (json) {
   console.dir(json)
-  return json.map(event => types[event.type] || silence)
+  return json.map(parseNote)
 }
 
 function writeNote (frequency, duration, start) {
@@ -92,11 +159,9 @@ function writeNote (frequency, duration, start) {
 
 function writeSong (notes) {
   console.dir(notes)
-
   const now = context.currentTime
 
   notes.reduce((prevEnd, info) => {
-    console.log(...info, prevEnd)
     writeNote(...info, prevEnd)
 
     const duration = info[1]
@@ -104,7 +169,25 @@ function writeSong (notes) {
   }, now)
 }
 
+function stop () {
+  return new Promise((resolve, reject) => {
+    const now = context.currentTime
+    amp.gain.cancelScheduledValues(now)
+    amp.gain.setValueAtTime(now, volume)
+    amp.gain.linearRampToValueAtTime(0, now + 3.0)
+
+    setTimeout(() => {
+      const now = context.currentTime
+      oscillator.frequency.cancelScheduledValues(now)
+      amp.gain.cancelScheduledValues(now)
+      resolve()
+    }, 3100)
+  })
+}
+
 playButton.addEventListener('click', event => {
+  playButton.disabled = true
+  stopButton.disabled = false
   context.suspend()
     .then(fetchEvents)
     .then(response => response.json())
@@ -114,7 +197,16 @@ playButton.addEventListener('click', event => {
     .then(() => console.log('should be playing'))
 })
 
-const repoRegexp = /^(\w+) ?\/ ?(\w+)$/
+stopButton.addEventListener('click', event => {
+  stopButton.disabled = true
+  stop()
+    .then(() => console.log('should not be playing'))
+    .then(() => context.suspend())
+    .then(() => { playButton.disabled = false })
+})
+stopButton.disabled = true
+
+const repoRegexp = /^([\w-]+) ?\/ ?([\w-]+)$/
 
 userField.addEventListener('paste', event => {
   const data = event.clipboardData.getData('Text')
@@ -127,3 +219,5 @@ userField.addEventListener('paste', event => {
     repoField.focus()
   }
 })
+
+context.suspend() // start out not playing
